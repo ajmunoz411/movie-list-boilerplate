fc4cc992e11931ce64a9fc79f9fd48ef179ca473
Andrew Munoz <ajmunoz411@gmail.com>
2021-05-13 18:40:36 -0700

    Implementation of 'to watch' button on movies and global buttons in progress

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,6 +13,7 @@ class App extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleWatchClick = this.handleWatchClick.bind(this);
   }
 
   handleChange(event) {
@@ -46,13 +47,39 @@ class App extends React.Component {
     event.preventDefault();
     var addedMovie = {};
     addedMovie.title = this.state.addMovie;
+    addedMovie.watched = false;
     this.state.movieData.push(addedMovie);
     // console.log('new movie Data', this.state.movieData);
     this.setState({
       movieData: movieData,
       addMovie: ''
     })
+  }
 
+  handleWatchClick(movie) {
+    console.log('movie', movie);
+    //check if toggled value is true or false
+    if (!movie.watched) {
+    // if toggled is true
+      // change toggle watch to 'watched'
+      // this.setState({
+      //   buttonText: 'Watched'
+      // })
+      movie.watched = true;
+    } else {
+      // this.setState({
+      //   buttonText: 'To Watch'
+      // })
+      movie.watched = false;
+    }
+    //else
+      // change toggle watch to 'to watch'
+    // change toggled to its opposite
+    this.setState({
+      toggled: !this.state.toggled
+    })
+
+    // console.log('event target', event.target);
   }
 
   render() {
@@ -68,8 +95,11 @@ class App extends React.Component {
           <input type="text" placeholder="Search..." name="search" onChange={this.handleChange} value={this.state.search}></input>
           <button>Go!</button>
         </form>
+        <br></br><hr />
+        <button>Watched</button><button>To Watch</button>
+        <br></br><hr />
         <div className="movieList">
-          {this.state.movieData.map((movie, index) => <MovieListEntry movie={movie} key={index} />)}
+          {this.state.movieData.map((movie, index) => <MovieListEntry movie={movie} key={index} handleWatchClick={this.handleWatchClick}/>)}
         </div>
       </div>
     )
--- client/src/components/MovieListEntry.jsx
+++ client/src/components/MovieListEntry.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 
-var MovieListEntry = (props) => (
-  <div>
-    <h2>{props.movie.title}</h2><button>To Watch</button><hr/>
-  </div>
-)
+// var MovieListEntry = (props) => (
+//   <div>
+//     <h2>{props.movie.title}</h2><button>To Watch</button><hr/>
+//   </div>
+// )
+
+class MovieListEntry extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      buttonText: 'To Watch',
+      toggled: true
+    }
+
+    this.handleWatchedClick = this.handleWatchedClick.bind(this);
+  }
+
+  handleWatchedClick(event) {
+    //check if toggled value is true or false
+    if (this.state.toggled) {
+    // if toggled is true
+      // change toggle watch to 'watched'
+      this.setState({
+        buttonText: 'Watched'
+      })
+    } else {
+      this.setState({
+        buttonText: 'To Watch'
+      })
+    }
+    //else
+      // change toggle watch to 'to watch'
+    // change toggled to its opposite
+    this.setState({
+      toggled: !this.state.toggled
+    })
+
+    // console.log('event target', event.target);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.movie.title}</h2><button onClick={() => {
+          this.props.handleWatchClick(this.props.movie);
+          this.handleWatchedClick;
+        }}>{this.state.buttonText}</button><hr/>
+     </div>
+    )
+  }
+
+}
 
 export default MovieListEntry;
